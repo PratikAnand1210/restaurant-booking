f9d50e1d3a01556f79662bb811b7c8e8fa0005c4
PratikAnand1210 <pratikanand1210@gmail.com>
2024-06-25 00:26:56 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">PRATIK</div>
+          <div className="right">
+            <p>NILDIH GOLMURI</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Created by Pratik</p>
+          </div>
+          <div className="right">
+            <p>Final year project</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
